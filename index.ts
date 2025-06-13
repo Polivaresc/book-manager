92c3d92628424f6b4e5b75e0625e1133a94017ed
Polivaresc <paulaolca98@gmail.com>
2025-06-13 21:19:23 +0200

    Added methods: removeBook, sortBook, findBookByAuthor, updateBook

--- index.ts
+++ index.ts
@@ -1,31 +1,47 @@
 import { BookManager } from "./src/BookManager";
 
-const BookManagerExample: BookManager = new BookManager([]);
+const bookManagerExample: BookManager = new BookManager([]);
 
-BookManagerExample.addBook({
+bookManagerExample.addBook({
     title: "The three body problem",
     author: "Cixin Liu",
     pages: 448
 })
 
-BookManagerExample.addBook({
+bookManagerExample.addBook({
     title: "The dark forest",
     author: "Cixin Liu",
     pages: 528
 })
 
-BookManagerExample.addBook({
+bookManagerExample.addBook({
     title: "Death's end",
     author: "Cixin Liu",
     pages: 608
 })
 
-BookManagerExample.addBook({
+bookManagerExample.addBook({
     title: "Frankenstein",
     author: "Mary Shelley",
     pages: 304
 })
 
-console.log(BookManagerExample.listBooks());
+console.log('List Of Books: ', bookManagerExample.listBooks());
 
-console.log(BookManagerExample.totalPages());
+console.log('Total Pages: ', bookManagerExample.totalPages());
+
+bookManagerExample.sortBooksByTitle()
+console.log('Books Sorted by Title: ', bookManagerExample.listBooks());
+
+bookManagerExample.sortBooksByPages(true)
+console.log('Books Sorted by Pages Descending: ', bookManagerExample.listBooks());
+
+bookManagerExample.sortBooksByPages(false)
+console.log('Books Sorted by Pages Ascending: ', bookManagerExample.listBooks());
+
+bookManagerExample.updateBook("Frankenstein", { author: "M. Shelley" });
+bookManagerExample.updateBook("The three body problem", { title: "The 3 body problem", pages: 500 });
+
+console.log('Updated list Of Books: ', bookManagerExample.listBooks());
+
+console.log('Removing book: ', bookManagerExample.removeBook("The Lord of the Rings"));
--- src/BookManager.ts
+++ src/BookManager.ts
@@ -13,7 +13,56 @@ export class BookManager {
         return this.books.find((book) => book.title === title);
     }
 
+    findBookByAuthor(author: string): Book[] {
+        return this.books.filter((book) => book.author === author);
+    }
+
     totalPages(): number {
         return this.books.reduce((total, book) => total += book.pages, 0);
     }
+
+    removeBook(title: string): boolean {
+        const index: number = this.books.findIndex((book) => book.title === title);
+        if (index === -1) return false;
+        this.books.splice(index, 1);
+        return true;
+    }
+
+    sortBooksByTitle(): void {
+        this.books.sort((a, b) => {
+            const a_title: string = a.title.toUpperCase();
+            const b_title: string = b.title.toUpperCase();
+            if (a_title < b_title) return -1;
+            if (a_title > b_title) return 1;
+            return 0;
+        })
+    }
+
+    sortBooksByPages(descending: boolean): void {
+        this.books.sort((a, b) => {
+            if (descending) {
+                if (a.pages < b.pages) return 1;
+                if (a.pages > b.pages) return -1;
+            } else {
+                if (a.pages < b.pages) return -1;
+                if (a.pages > b.pages) return 1;
+            }
+            return 0;
+        })
+    }
+
+    updateBook(title: string, updatedBook: Partial<Book>): boolean {
+        let book: Book | undefined = this.findBookByTitle(title);
+        if (book === undefined) return false;
+        if ('title' in updatedBook && updatedBook.title !== undefined) {
+            book.title = updatedBook.title;
+        }
+        if ('author' in updatedBook && updatedBook.author !== undefined) {
+            book.author = updatedBook.author;
+        }
+        if ('pages' in updatedBook && updatedBook.pages !== undefined) {
+            book.pages = updatedBook.pages;
+        }
+        return true;
+    } 
 }
